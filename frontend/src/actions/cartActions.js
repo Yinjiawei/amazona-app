cdcf7084ce5c0214878f199a1e850e4bef26b932
jiawei.yin <jiawei.yin@hotmail.com>
2021-10-12 14:59:14 +0800

    add payment screnn

--- frontend/src/actions/cartActions.js
+++ frontend/src/actions/cartActions.js
@@ -2,6 +2,7 @@ import Axios from 'axios';
 import {
     CART_ADD_ITEM,
     CART_REMOVE_ITEM,
+    CART_SAVE_PAYMENT_METHOD,
     CART_SAVE_SHIPPING_ADDRESS
 } from "../constants/cartConstants";
 
@@ -19,7 +20,7 @@ export const addToCart = (productId, qty) => async (dispatch, getState) => {
         }
     })
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
-}
+};
 
 export const removeFromCart = (productId) => async (dispatch, getState) => {
     dispatch({
@@ -27,9 +28,13 @@ export const removeFromCart = (productId) => async (dispatch, getState) => {
         payload: productId
     })
     localStorage.setItem('cartItems', JSON.stringify(getState().cart.cartItems));
-}
+};
 
 export const saveShippingAddress = (data) => (dispatch) => {
-    dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, paylod: data });
+    dispatch({ type: CART_SAVE_SHIPPING_ADDRESS, payload: data });
     localStorage.setItem('shippingAddress', JSON.stringify(data));
-}
+};
+
+export const savePaymentMethod = (data) => (dispatch) => {
+    dispatch({ type: CART_SAVE_PAYMENT_METHOD, payload: data });
+};
