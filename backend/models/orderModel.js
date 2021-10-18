d94f4a047de545d4b242f7d6764b11ad9b1c6101
jiawei.yin <jiawei.yin@hotmail.com>
2021-10-18 11:02:16 +0800

    create place order API

---
+++ backend/models/orderModel.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose';
+
+const orderSchema = new mongoose.Schema(
+    {
+        orderItem: [
+            {
+                name: { type: String, required: true },
+                qty: { type: Number, required: true },
+                image: { type: String, required: true },
+                price: { type: Number, required: true },
+                product: { type: mongoose.Types.ObjectId, ref: 'Product', required: true },
+            }
+        ],
+        shippingAddress: {
+            fullName: { type: String, required: true },
+            address: { type: String, required: true },
+            city: { type: String, required: true },
+            postalCode: { type: String, required: true },
+            country: { type: String, required: true },
+
+        },
+        paymentMethod: { type: String, required: true },
+        itemPrice: { type: Number, required: true },
+        shippingPrice: { type: Number, required: true },
+        taxPrice: { type: Number, required: true },
+        totalPrice: { type: Number, required: true },
+        user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+        ispaid: { tuype: Boolean, default: false },
+        paidAt: { type: Date },
+        isDelivered: { tuype: Boolean, default: false },
+        deliveredAt: { type: Date },
+    },
+    {
+        timestamps: true,
+    }
+);
+const Order = mongoose.model("Order", orderSchema);
+
+export default Order;
---
+++ backend/routers/orderRouter.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import Order from '../models/orderModel.js'
+import { isAuth } from '../utils.js';
+
+const orderRouter = express.Router();
+
+orderRouter.post(
+    '/',
+    isAuth,
+    expressAsyncHandler(async (req, res) => {
+        if (req.body.orderItem.length === 0) {
+            res.status(400).send({ message: 'Cart is empty' });
+        } else {
+            const order = new Order({
+                orderItem: req.body.orderItems,
+                shippingAddress: req.body.shippingAddress,
+                paymentMethod: req.body.paymentMethod,
+                itemPrice: req.body.itemPrice,
+                shippingPrice: req.body.shippingPrice,
+                taxPrice: req.body.taxPrice,
+                totalPrice: req.body.totalPrice,
+                user: req.user._id,
+            });
+            const createOrder = await order.save();
+            res.status(200).send({ message: 'New Oder Created', order: createOrder });
+        }
+    })
+);
+
+export default orderRouter;
